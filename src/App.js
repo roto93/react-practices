ddc10ebb96ad997d774b3bda8fe59362b05e2e5f
Apple <roto93@yahoo.com.tw>
2021-04-20 02:05:59 +0800

    start layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={'container'}>
+        <div className='title'>
+          <img src={require('../public/react-logo.png')} alt="logo" />
+          <h1>Welcome</h1>
+        </div>
+        <div className="content"></div>
+      </div>
     </div>
   );
 }
